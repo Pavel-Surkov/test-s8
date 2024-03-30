d44221e105c498edc2a47f66c5037b08ec98e3e8
Pavel-Surkov <surkovv.pavel@gmail.com>
2024-03-30 14:37:24 +0700

    init project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// Лучше всего реализовать выбор количества подгржаемых элементов через select, но в задании написано через инпут
+const MIN_ELEMENTS_COUNT = 1;
+const MAX_ELEMENTS_COUNT = 20;
+const INITIAL_ELEMENTS_COUNT = 10;
+
+function App() {
+  return (
+    <main className="flex flex-col items-center p-10 min-h-[100vh]"></main>
+  );
+}
+
+export default App;
